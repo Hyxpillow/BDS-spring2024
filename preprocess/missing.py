b14cbe5e53e2b45b3f18a8e5a2dac685ca6b5ad5
Hyxpillow <hyxpillow@hyxpillow.local>
2024-05-07 17:31:54 -0400

    update age

--- preprocess/missing.py
+++ preprocess/missing.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def remove_missing_value(df: pd.DataFrame):
     df = df[df["core_actor"].notna()]
+    df = df[df["actor_avg_age"] > 10]
     df.to_csv("datasets/clean.csv", index=False)
     df = pd.read_csv("datasets/clean.csv")
 
